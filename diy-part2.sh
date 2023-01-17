2c73aa41f9f78ce945928cfc698e859aa7f96ccd
s890214 <sp8902142008@126.com>
2023-01-17 16:05:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
