b938044538909b80f1c30873d82704eec84163e2
s890214 <sp8902142008@126.com>
2023-03-31 20:54:57 +0800

    Update diy-part2.sh

--- MT798X-H/diy-part2.sh
+++ MT798X-H/diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
+rm -rf feeds/packages/net/miniupnpd && svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/miniupnpd feeds/packages/net/miniupnpd
