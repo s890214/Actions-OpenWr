8980725009198e8c488ccac8545a215075d0f37c
s890214 <sp8902142008@126.com>
2023-09-08 17:01:53 +0800

    Update diy-part1.sh

--- MT798X-H/diy-part1.sh
+++ MT798X-H/diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
