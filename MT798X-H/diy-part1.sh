9e7c6ed3ab053517bf5aa1604f8ce991c61967a6
s890214 <sp8902142008@126.com>
2024-02-22 15:41:03 +0800

    Update diy-part1.sh

--- MT798X-H/diy-part1.sh
+++ MT798X-H/diy-part1.sh
@@ -17,4 +17,3 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon' >>feeds.conf.default
-git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
