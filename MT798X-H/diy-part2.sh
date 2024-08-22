e7d25e56d5ba62ea1b08ac42b41dafe913dfa322
s890214 <sp8902142008@126.com>
2024-08-22 15:44:34 +0800

    Update diy-part2.sh

--- MT798X-H/diy-part2.sh
+++ MT798X-H/diy-part2.sh
@@ -16,13 +16,9 @@
 # rm -rf feeds/packages/net/miniupnpd && svn co https://github.com/immortalwrt/packages/tree/15a48bbdb265f0df4f2e517b3b53c1cd00878329/net/miniupnpd feeds/packages/net/miniupnpd
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
-git clone https://github.com/s890214/openwrt-packages /tmp/openwrt-packages
-cd /tmp/openwrt-packages
+mkdir -p files/tmp
+git clone https://github.com/s890214/openwrt-packages files/tmp/
 
-# 安装下载的 ipk 文件
-opkg install ./lucky_2.11.2_Openwrt_arm64.ipk
-opkg install ./luci-app-lucky_2.1.1-1_all.ipk
-opkg install ./luci-i18n-lucky-zh-cn_2.1.1-1_all.ipk
-
-# 检查安装是否成功
-opkg list-installed | grep lucky
+echo "opkg install /tmp/lucky_2.11.2_Openwrt_arm64.ipk" >> files/etc/rc.local
+echo "opkg install /tmp/luci-app-lucky_2.1.1-1_all.ipk" >> files/etc/rc.local
+echo "opkg install /tmp/luci-i18n-lucky-zh-cn_2.1.1-1_all.ipk" >> files/etc/rc.local
