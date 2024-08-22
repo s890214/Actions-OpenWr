713710c0724f5e579fb0927c98784f8262b9b93c
s890214 <sp8902142008@126.com>
2024-08-22 15:27:27 +0800

    Update diy-part2.sh

--- MT798X-H/diy-part2.sh
+++ MT798X-H/diy-part2.sh
@@ -15,4 +15,14 @@
 # git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
 # rm -rf feeds/packages/net/miniupnpd && svn co https://github.com/immortalwrt/packages/tree/15a48bbdb265f0df4f2e517b3b53c1cd00878329/net/miniupnpd feeds/packages/net/miniupnpd
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-# git clone https://github.com/s890214/openwrt-packages.git package/lucky
+
+git clone https://github.com/s890214/openwrt-packages /tmp/openwrt-packages
+cd /tmp/openwrt-packages
+
+# 安装下载的 ipk 文件
+opkg install ./lucky_2.11.2_Openwrt_arm64.ipk
+opkg install ./luci-app-lucky_2.1.1-1_all.ipk
+opkg install ./luci-i18n-lucky-zh-cn_2.1.1-1_all.ipk
+
+# 检查安装是否成功
+opkg list-installed | grep lucky
