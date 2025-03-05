b06aeb5b3ba9600208533cdff0df3a1a526dce72
s890214 <sp8902142008@126.com>
2025-03-05 21:19:24 +0800

    Update diy-part2.sh

--- MT798X-H/diy-part2.sh
+++ MT798X-H/diy-part2.sh
@@ -17,31 +17,32 @@
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # ## -------------- passwall ---------------------------
-# git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 
 # ## -------------- lucky ---------------------------
 rm -rf feeds/packages/net/lucky
 rm -rf feeds/luci/applications/luci-app-lucky
 
 # #/etc/config/lucky.daji/lucky.conf
-git clone https://github.com/gdy666/luci-app-lucky.git -b main package/diy/lucky
+# git clone https://github.com/gdy666/luci-app-lucky.git -b main package/diy/lucky
+git clone -b v2.15.7 --single-branch https://github.com/gdy666/luci-app-lucky.git package/diy/lucky
 sleep 1
 
 #-- use custom binary ver
 # cp -f ${GITHUB_WORKSPACE}/_modFiles/2lucky/luckyMakefile package/diy/lucky/lucky/Makefile
-curl -o package/diy/lucky/lucky/Makefile https://raw.githubusercontent.com/budaig/Actions-OpenWrt/main/_modFiles/2lucky/luckyMakefile
-if [ $? -eq 0 ]; then
-    echo "luckyMakefile copied"
-else
-    echo "luckyMakefile copy failed"
-fi
+# curl -o package/diy/lucky/lucky/Makefile https://raw.githubusercontent.com/budaig/Actions-OpenWrt/main/_modFiles/2lucky/luckyMakefile
+# if [ $? -eq 0 ]; then
+#     echo "luckyMakefile copied"
+# else
+#     echo "luckyMakefile copy failed"
+# fi
 
 # cp -f ${GITHUB_WORKSPACE}/_modFiles/2lucky/lucky package/diy/lucky/lucky/files/lucky
 # curl -o package/diy/lucky/lucky/files/lucky https://raw.githubusercontent.com/budaig/Actions-OpenWrt/main/_modFiles/2lucky/lucky
-curl -o package/diy/lucky/lucky/files/lucky https://raw.githubusercontent.com/budaig/Actions-OpenWrt/main/_modFiles/2lucky/luckyMakefile250220andnewer
-if [ $? -eq 0 ]; then
-    echo "lucky bin copied"
-else
-    echo "lucky bin copy failed"
-fi
+# curl -o package/diy/lucky/lucky/files/lucky https://raw.githubusercontent.com/budaig/Actions-OpenWrt/main/_modFiles/2lucky/luckyMakefile250220andnewer
+# if [ $? -eq 0 ]; then
+#     echo "lucky bin copied"
+# else
+#     echo "lucky bin copy failed"
+# fi
 # ## ---------------------------------------------------------
